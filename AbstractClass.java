8bee8d9f55e49476b6e3dcd65ddb0f2382d83dad
Sam <168297366+SamuelDavidR90@users.noreply.github.com>
2024-08-01 14:30:02 -0500

    Update AbstractClass.java

--- AbstractClass.java
+++ AbstractClass.java
@@ -17,8 +17,11 @@ public class AbstractClass {
         Square square = new Square();
         double resultSquare = square.toCalculateArea();
         
+        Triangle triangle = new Triangle();
+        double resultTriangle = triangle.toCalculateArea();
         
-        
+        Pentagon pentagon = new Pentagon();
+        double resultPentagon = pentagon.toCalculateArea();
         
         System.out.println("Do you want to continue ? Type Yes or Not");
         Scanner keyboardString = new Scanner(System.in);
@@ -27,7 +30,7 @@ public class AbstractClass {
         //repetitive structure while and conditional switch inside
         while(answer.equalsIgnoreCase("yes")){ 
         //Introducing values by keyboard using Scanner
-        System.out.println("To Calculate Area: press \n 1 Circle and \n 2 Square");
+        System.out.println("To Calculate Area: press \n 1 Circle and \n 2 Square \n 3 Triangle \n 4 Pentagon");
         Scanner keyboardInt = new Scanner(System.in);
         int optionSelected = keyboardInt.nextInt();
         
@@ -35,6 +38,10 @@ public class AbstractClass {
             case 1: System.out.println("Area of Circle: " +  resultCircle);
             break;
             case 2: System.out.println("Area of Square: " +  resultSquare);
+            break;
+            case 3: System.out.println("Area of Square: " +  resultTriangle);
+            break;
+            case 4: System.out.println("Area of Square: " +  resulPentagon);
             break;
             default: System.out.println("Introduce a valid number");
             }
